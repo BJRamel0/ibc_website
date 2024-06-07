43c5a6194d2d88d0c9800339a33f28d5403971fc
Bjramel0 <Bramel@hawaii.edu>
2024-06-07 15:14:50 +0800

    xxx

---
+++ public/home.js
@@ -0,0 +1,11 @@
+// home.js
+
+function toggleNavMenu() {
+    var x = document.getElementById("mobileNav");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else {
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
+  
